cb07b74a95193df7bdb62f5b5629f48c092d5890
Peter Johnson <johnson.peter@gmail.com>
2014-10-06 00:10:33 -0700

    Update test.py to use Joystick.

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import wpilib
 
 class MyRobot(wpilib.IterativeRobot):
     def robotInit(self):
+        self.lstick = wpilib.Joystick(1)
         self.m1 = wpilib.Victor(1)
 
     def teleopPeriodic(self):
-        self.m1.set(0.5)
+        self.m1.set(self.lstick.getY())
 
 if __name__ == "__main__":
     wpilib.RobotBase.main(MyRobot)
