8b147b9e71a3cd956dc32dadce02b4b027bc6343
Peter Johnson <johnson.peter@gmail.com>
2014-10-07 00:56:46 -0700

    Add DoubleSolenoid to test.py.

--- test.py
+++ test.py
@@ -6,14 +6,19 @@ class MyRobot(wpilib.IterativeRobot):
         self.lstick = wpilib.Joystick(1)
         self.rstick = wpilib.Joystick(2)
         self.cstick = wpilib.Joystick(3)
-        self.m1 = wpilib.Victor(1)
-        self.m2 = wpilib.Victor(2)
-        self.m3 = wpilib.Victor(3)
+        self.m1 = wpilib.Victor(0)
+        self.m2 = wpilib.Victor(1)
+        self.m3 = wpilib.Victor(2)
         self.drive = wpilib.RobotDrive(self.m1, self.m2)
+        self.shifter = wpilib.DoubleSolenoid(0, 1)
 
     def teleopPeriodic(self):
         self.drive.tankDrive(self.lstick, self.rstick)
         self.m3.set(self.cstick.getY())
+        if self.lstick.getTrigger():
+            self.shifter.set(wpilib.DoubleSolenoid.Value.kForward)
+        if self.rstick.getTrigger():
+            self.shifter.set(wpilib.DoubleSolenoid.Value.kReverse)
 
 if __name__ == "__main__":
     wpilib.RobotBase.main(MyRobot)
