e42e3675b4983da85a4a750453e152162027031c
Peter Johnson <johnson.peter@gmail.com>
2014-10-14 23:37:25 -0700

    Finish PWM tests except SmartDashboard functions.

--- wpilib/tests/test_pwm.py
+++ wpilib/tests/test_pwm.py
@@ -37,20 +37,90 @@ def test_pwm_free(pwm, hal):
     hal.freePWMChannel.assert_called_once_with(wasport)
     hal.freeDIO.assert_called_once_with(wasport)
 
-def test_pwm_enableDeadbandElimination(pwm):
-    pwm.enableDeadbandElimination(True)
-    assert pwm.eliminateDeadband == True
-    pwm.enableDeadbandElimination(False)
-    assert pwm.eliminateDeadband == False
+@pytest.mark.parametrize("value", [True, False])
+def test_pwm_enableDeadbandElimination(value, pwm):
+    pwm.enableDeadbandElimination(value)
+    assert pwm.eliminateDeadband == value
+
+def test_pwm_setBounds(pwm, hal, wpilib):
+    hal.getLoopTiming.return_value = wpilib.SensorBase.kSystemClockTicksPerMicrosecond
+    # use victor settings for test
+    pwm.setBounds(2.027, 1.525, 1.507, 1.49, 1.026)
+    assert pwm.maxPwm == 1526
+    assert pwm.deadbandMaxPwm == 1024
+    assert pwm.centerPwm == 1005
+    assert pwm.deadbandMinPwm == 989
+    assert pwm.minPwm == 525
+
+@pytest.fixture(scope="function")
+def boundpwm(pwm):
+    # mock up with nice numeric values
+    pwm.maxPwm = 1500
+    pwm.deadbandMaxPwm = 1050
+    pwm.centerPwm = 1000
+    pwm.deadbandMinPwm = 950
+    pwm.minPwm = 500
+    return pwm
 
 def test_pwm_getChannel(pwm):
     assert pwm.getChannel() == pwm.channel
+
+@pytest.mark.parametrize("param,expected", [(0.5, 1000), (0.25, 750)])
+def test_setPosition(param, expected, boundpwm, hal):
+    boundpwm.setPosition(param)
+    hal.setPWM.assert_called_once_with(boundpwm._port, expected)
+
+@pytest.mark.parametrize("param,expected", [(1.5, 1500), (-0.5, 500)])
+def test_setPosition_limits(param, expected, boundpwm, hal):
+    boundpwm.setPosition(param)
+    hal.setPWM.assert_called_once_with(boundpwm._port, expected)
+
+@pytest.mark.parametrize("param,expected", [(1000, 0.5), (750, 0.25)])
+def test_getPosition(param, expected, boundpwm, hal):
+    hal.getPWM.return_value = param
+    assert boundpwm.getPosition() == expected
+
+@pytest.mark.parametrize("param,expected", [(1600, 1.0), (400, 0.0)])
+def test_getPosition_limits(param, expected, boundpwm, hal):
+    hal.getPWM.return_value = param
+    assert boundpwm.getPosition() == expected
+
+@pytest.mark.parametrize("db,param,expected",
+        # no deadband elimination results in direct scaling from center
+        [(False, 0.0, 1000), (False, 0.5, 1251), (False, -0.5, 750),
+        # deadband elimination results in scaling from deadband edge
+         (True, 0.0, 1000), (True, 0.5, 1275), (True, -0.5, 725)])
+def test_setSpeed(db, param, expected, boundpwm, hal):
+    boundpwm.eliminateDeadband = db
+    boundpwm.setSpeed(param)
+    hal.setPWM.assert_called_once_with(boundpwm._port, expected)
+
+@pytest.mark.parametrize("param,expected", [(1.5, 1500), (-1.5, 500)])
+def test_setSpeed_limits(param, expected, boundpwm, hal):
+    boundpwm.setSpeed(param)
+    hal.setPWM.assert_called_once_with(boundpwm._port, expected)
+
+@pytest.mark.parametrize("db,param,expected",
+        # no deadband elimination results in direct scaling from center
+        [(False, 1251, 0.5), (False, 750, -0.5),
+        # deadband elimination results in scaling from deadband edge
+         (True, 1275, 0.5), (True, 725, -0.5)])
+def test_getSpeed(db, param, expected, boundpwm, hal):
+    # no deadband elimination
+    boundpwm.eliminateDeadband = db
+    hal.getPWM.return_value = param
+    assert round(boundpwm.getSpeed(), 2) == expected
+
+@pytest.mark.parametrize("param,expected", [(1600, 1.0), (400, -1.0)])
+def test_getSpeed_limits(param, expected, boundpwm, hal):
+    hal.getPWM.return_value = param
+    assert boundpwm.getSpeed() == expected
 
 def test_pwm_setRaw(pwm, hal):
     pwm.setRaw(60)
     hal.setPWM.assert_called_once_with(pwm._port, 60)
 
-def test_pwm_setRaw_error(pwm, hal):
+def test_pwm_setRaw_freed(pwm, hal):
     pwm.free()
     hal.reset_mock()
     with pytest.raises(ValueError):
@@ -61,10 +131,70 @@ def test_pwm_getRaw(pwm, hal):
     assert pwm.getRaw() == hal.getPWM.return_value
     hal.getPWM.assert_called_once_with(pwm._port)
 
-def test_pwm_getRaw_error(pwm, hal):
+def test_pwm_getRaw_freed(pwm, hal):
     pwm.free()
     hal.reset_mock()
     with pytest.raises(ValueError):
         pwm.getRaw()
     assert not hal.getPWM.called
+
+@pytest.mark.parametrize("param,expected", [("k4X", 3), ("k2X", 1), ("k1X", 0)])
+def test_pwm_setPeriodMultiplier(param, expected, pwm, hal):
+    pwm.setPeriodMultiplier(getattr(pwm.PeriodMultiplier, param))
+    hal.setPWMPeriodScale.assert_called_once_with(pwm._port, expected)
+
+def test_pwm_setPeriodMultiplier_freed(pwm, hal):
+    pwm.free()
+    with pytest.raises(ValueError):
+        pwm.setPeriodMultiplier(pwm.PeriodMultiplier.k4X)
+    assert not hal.setPWMperiodScale.called
+
+def test_pwm_setPeriodMultiplier_badvalue(pwm, hal):
+    with pytest.raises(ValueError):
+        pwm.setPeriodMultiplier(5)
+    assert not hal.setPWMperiodScale.called
+
+def test_pwm_setZeroLatch(pwm, hal):
+    pwm.setZeroLatch()
+    hal.latchPWMZero.assert_called_once_with(pwm._port)
+
+def test_pwm_setZeroLatch_freed(pwm, hal):
+    pwm.free()
+    with pytest.raises(ValueError):
+        pwm.setZeroLatch()
+    assert not hal.latchPWMZero.called
+
+def test_pwm_getMaxPositivePwm(boundpwm):
+    assert boundpwm.getMaxPositivePwm() == 1500
+
+@pytest.mark.parametrize("db,expected", [(True, 1050), (False, 1001)])
+def test_pwm_getMinPositivePwm(db, expected, boundpwm):
+    boundpwm.eliminateDeadband = db
+    assert boundpwm.getMinPositivePwm() == expected
+
+def test_pwm_getCenterPwm(boundpwm):
+    assert boundpwm.getCenterPwm() == 1000
+
+@pytest.mark.parametrize("db,expected", [(True, 950), (False, 999)])
+def test_pwm_getMaxNegativePwm(db, expected, boundpwm):
+    boundpwm.eliminateDeadband = db
+    assert boundpwm.getMaxNegativePwm() == expected
+
+def test_pwm_getMinNegativePwm(boundpwm):
+    assert boundpwm.getMinNegativePwm() == 500
+
+@pytest.mark.parametrize("db,expected", [(True, 450), (False, 499)])
+def test_pwm_getPositiveScaleFactor(db, expected, boundpwm):
+    boundpwm.eliminateDeadband = db
+    assert boundpwm.getPositiveScaleFactor() == expected
+
+@pytest.mark.parametrize("db,expected", [(True, 450), (False, 499)])
+def test_pwm_getNegativeScaleFactor(db, expected, boundpwm):
+    boundpwm.eliminateDeadband = db
+    assert boundpwm.getNegativeScaleFactor() == expected
+
+@pytest.mark.parametrize("db,expected", [(True, 1000), (False, 1000)])
+def test_pwm_getFullRangeScaleFactor(db, expected, boundpwm):
+    boundpwm.eliminateDeadband = db
+    assert boundpwm.getFullRangeScaleFactor() == expected
 
