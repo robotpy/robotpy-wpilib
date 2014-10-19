7516b71f617ffd756ab540b95a8da22ba4f7c19e
Peter Johnson <peter@tortall.net>
2014-10-19 10:54:47 -0700

    Add docstrings to conftest fixtures.

--- wpilib/tests/conftest.py
+++ wpilib/tests/conftest.py
@@ -3,14 +3,17 @@ from unittest.mock import MagicMock, patch
 
 @pytest.fixture(scope="function")
 def hal(request):
+    """Magic-mock for hal module."""
     return MagicMock(name='mock_hal')
 
 @pytest.fixture(scope="function")
 def frccan(request):
+    """Magic-mock for frccan module."""
     return MagicMock(name='mock_frccan')
 
 @pytest.fixture(scope="function")
 def wpilib(request, hal, frccan):
+    """Monkeypatches sys.modules hal and frccan and loads wpilib."""
     # need to monkey patch sys.modules so wpilib can load these
     m = patch.dict('sys.modules', {'frccan': frccan, 'hal': hal})
     m.start()
