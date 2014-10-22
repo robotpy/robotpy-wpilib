4773ee03585c77f743a470de8e4538792a30a965
Peter Johnson <peter@tortall.net>
2014-10-21 20:22:50 -0700

    Add constants for hal and frccan.

--- wpilib/tests/conftest.py
+++ wpilib/tests/conftest.py
@@ -4,12 +4,35 @@ from unittest.mock import MagicMock, patch
 @pytest.fixture(scope="function")
 def hal(request):
     """Magic-mock for hal module."""
-    return MagicMock(name='mock_hal')
+    hal = MagicMock(name='mock_hal')
+    hal.kHALAllianceStationID_red1 = 0
+    hal.kHALAllianceStationID_red2 = 1
+    hal.kHALAllianceStationID_red3 = 2
+    hal.kHALAllianceStationID_blue1 = 3
+    hal.kHALAllianceStationID_blue2 = 4
+    hal.kHALAllianceStationID_blue3 = 5
+    hal.kMaxJoystickAxes = 12
+    hal.kMaxJoystickPOVs = 12
+    return hal
 
 @pytest.fixture(scope="function")
 def frccan(request):
-    """Magic-mock for frccan module."""
-    return MagicMock(name='mock_frccan')
+    """Mock for frccan module."""
+    frccan = MagicMock(name='mock_frccan')
+
+    class CANError(RuntimeError):
+        pass
+    class CANMessageNotFound(CANError):
+        pass
+
+    frccan.CANError = CANError
+    frccan.CANMessageNotFound = CANMessageNotFound
+
+    # Flags in the upper bits of the messageID
+    frccan.CAN_IS_FRAME_REMOTE = 0x80000000
+    frccan.CAN_IS_FRAME_11BIT = 0x40000000
+
+    return frccan
 
 @pytest.fixture(scope="function")
 def wpilib(request, hal, frccan):
