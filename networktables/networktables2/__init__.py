586455ce6cfacf4b87878c2fe823bba9e3ad968a
Dustin Spicuzza <dustin@virtualroadside.com>
2014-11-21 13:35:37 -0500

    Move networktables to its own repo

--- networktables/networktables2/__init__.py
+++
@@ -1,9 +0,0 @@
-from .client import NetworkTableClient
-from .server import NetworkTableServer
-from .socketstream import SocketStreamFactory, SocketServerStreamProvider
-from .type import BooleanArray, NumberArray, StringArray
-
-try:
-    from .version import __version__
-except ImportError:
-    __version__ = 'master'
