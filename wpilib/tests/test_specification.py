7057eeb2e972498017e5bb91b7a2369fb9988d50
Dustin Spicuzza <dustin@virtualroadside.com>
2014-11-25 11:52:19 -0500

    Make message part of the assert

--- wpilib/tests/test_specification.py
+++ wpilib/tests/test_specification.py
@@ -14,11 +14,10 @@ def test_compare_file_specs(wpilib):
     assert exists(path), "WPILIB_JAVA_DIR does not point to wpilibj root dir"
 
 
-    from . import java_scanner
+    import java_scanner
 
     output = java_scanner.compare_folders(wpilib, [path])
 
     for item in output:
         if not item["matches"] and not item["ignored"]:
-            print("Error: item {} doesn't match java spec, and is not ignored.".format(item["name"]))
-            assert False
+            assert False, "Error: item {} doesn't match java spec, and is not ignored.".format(item["name"])
