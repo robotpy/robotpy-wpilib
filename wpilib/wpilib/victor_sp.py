43391192709a13df18ef02989d44217f4129c479
Peter Johnson <johnson.peter@gmail.com>
2014-12-09 22:07:15 -0800

    Fix VictorSP.
    Inheriting doesn't bring across underscored functions like __init__().

--- wpilib/wpilib/victor_sp.py
+++ wpilib/wpilib/victor_sp.py
@@ -1,7 +1,10 @@
-
 from .talon import Talon
+
+__all__ = ["VictorSP"]
 
 class VictorSP(Talon):
     """
         Vex Robotics Victor SP Speed Controller
     """
+    def __init__(self, channel):
+        super().__init__(channel)
