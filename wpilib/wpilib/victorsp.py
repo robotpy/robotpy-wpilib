523cdb4f0dc574aaef9429ec1dfdf95c85218791
Peter Johnson <johnson.peter@gmail.com>
2014-12-09 22:27:02 -0800

    VictorSP: Add documentation for constructor.

--- wpilib/wpilib/victorsp.py
+++ wpilib/wpilib/victorsp.py
@@ -6,5 +6,11 @@ class VictorSP(Talon):
     """
         Vex Robotics Victor SP Speed Controller
     """
+
     def __init__(self, channel):
+        """Constructor.
+
+        :param channel: The PWM channel that the VictorSP is attached to.
+
+        """
         super().__init__(channel)
