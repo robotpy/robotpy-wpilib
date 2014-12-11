06ce7022b56f766dde624b0e842850eb1426a898
Christian J. Balcom <robot.inventor@gmail.com>
2014-12-11 11:53:12 -0500

    Finished hal scanner, added unit test.

--- wpilib/tests/test_specification.py
+++ wpilib/tests/test_specification.py
@@ -6,16 +6,16 @@ import pytest
 
 @pytest.mark.skipif(os.getenv('WPILIB_JAVA_DIR', None) == None,
                     reason="Must specify WPILIB_JAVA_DIR environment variable to run this test")
-def test_compare_java_specs_wpilib(wpilib):
+def test_check_wpilib_api(wpilib):
 
 
     path = os.environ['WPILIB_JAVA_DIR']
     path = join(path, 'wpilibJavaDevices', 'src', 'main', 'java', 'edu', 'wpi', 'first', 'wpilibj')
     assert exists(path), "WPILIB_JAVA_DIR does not point to wpilibj root dir"
 
-    from spec_scanners import java_scanner
+    from spec_scanners import wpilib_scanner
 
-    output = java_scanner.compare_folders(wpilib, [path])
+    output = wpilib_scanner.compare_folders(wpilib, [path])
 
     for item in output["children"]:
         if item["errors"] > 0:
@@ -24,20 +24,20 @@ def test_compare_java_specs_wpilib(wpilib):
 
 @pytest.mark.skipif(os.getenv('HAL_DIR', None) == None,
                     reason="Must specify HAL_DIR environment variable to run this test")
-def test_compare_cpp_specs_hal(hal):
+def test_check_hal_api(hal):
 
 
     path = os.environ['HAL_DIR']
     path = join(path, 'include', 'HAL')
     assert exists(path), "HAL_DIR does not point to HAL root dir"
 
-    from spec_scanners import cpp_scanner
+    from spec_scanners import hal_scanner
 
-    output = cpp_scanner.compare_folders(hal, [path])
-
-    for item in output["methods"]:
+    frontend_output = hal_scanner.compare_header_dirs(hal, [path])
+    backend_output = hal_scanner.scan_c_end(hal, frontend_output)
+    for item in backend_output["methods"]:
         if item["errors"] > 0:
-            assert False, "Error: method {} doesn't match java spec, and is not ignored.".format(item["name"])
-    for item in output["classes"]:
+            assert False, "Error: method call to {} doesn't match c++ spec.".format(item["name"])
+    for item in backend_output["classes"]:
         if item["errors"] > 0:
-            assert False, "Error: class {} doesn't match java spec, and is not ignored.".format(item["name"])
+            assert False, "Error: class {} doesn't match c++ spec, and is not ignored.".format(item["name"])
