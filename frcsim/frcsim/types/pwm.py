93fc0c1f5090343220292e5e6981fcdec63fd54c
Dustin Spicuzza <dustin@virtualroadside.com>
2014-12-22 01:01:50 -0500

    Fix pwm

--- frcsim/frcsim/types/pwm.py
+++ frcsim/frcsim/types/pwm.py
@@ -2,8 +2,6 @@
 from ..controller import Controller
 from ..msgs.float64_pb2 import Float64
 
-from hal_impl import pwm_helpers
-
 import logging
 logger = logging.getLogger('gazebo.pwm')
 
@@ -15,22 +13,12 @@ class SimPWM:
         self.publisher = Controller.get().advertise('simulator/pwm/%s' % channel,
                                                     'gazebo.msgs.Float64')
         
-        # Wait for the type to be set, and determine which PWM reverse
-        # function to use from that
-        def _on_type_set(key, value):
-            if value != 'jaguar' and value != 'talon' and value != 'victor':
-                logger.warn("Simulation cannot handle unknown pwm type '%s' on channel %s" % (value, channel))
-                return
-            
-            logger.info("Registered %s device on channel %s" % (value, channel))
-            pwm_dict.register('value', self.on_value_changed, notify=True)
+        logger.info("Registered PWM device on channel %s" % channel)
+        pwm_dict.register('value', self.on_value_changed, notify=True)
         
-        pwm_dict.register('type', _on_type_set)
-        self.pwm_entry = pwm_dict
 
     def on_value_changed(self, key, value):
         msg = Float64()
-        msg.data = self.pwm_entry[value]
+        msg.data = value
         self.publisher.publish(msg)
 
- 
