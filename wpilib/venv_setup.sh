534f7d96954dd926ae480271c8fd4bac03ec197e
Dustin Spicuzza <dustin@virtualroadside.com>
2015-01-02 00:48:06 -0500

    Add Travis configuration

--- wpilib/venv_setup.sh
+++ wpilib/venv_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Set up virtualenv for Python 3 and pytest.
+# -> On Linux/OSX, consider using virtualenv-wrapper instead
 
+cd `dirname $0`
 virtualenv -p /usr/bin/python3 venv
 source venv/bin/activate
-pip install pytest-cov
+pip install -r testing-requirements.txt
 pip install -e .
