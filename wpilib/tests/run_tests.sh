150ec983b2c04bcc272412c69c84fe264536ae91
Dustin Spicuzza <dustin@virtualroadside.com>
2015-01-12 00:57:52 -0500

    Run pyfrc based tests as part of unit test regimen

--- wpilib/tests/run_tests.sh
+++ wpilib/tests/run_tests.sh
@@ -3,6 +3,39 @@
 set -e
 cd `dirname $0`
 
-PYTHONPATH=..:../../hal-sim:../../hal-base python3 -m coverage run --source wpilib -m pytest $@
+abspath() {
+	echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+# Not strictly necessary if testing-requirements was installed...
+PYTHONPATH=$(abspath ./..):$(abspath ../../hal-sim):$(abspath ../../hal-base):$PYTHONPATH
+
+python3 -m coverage run --source wpilib -m pytest $@
 python -m coverage report -m
+
+
+EXAMPLES="
+	iterative/
+	sample/
+	test/
+	
+	examples/GettingStarted/
+	examples/MecanumDrive/
+	examples/MotorControl/
+	
+	examples/QuickVision/
+	examples/TankDrive/
+"
+
+# Doesn't work right now:
+# - examples/GearsBot/
+# - examples/pacgoat/
+
+if [ ! "$NOPYFRC" == "" ]; then
+	for example in $EXAMPLES; do
+		pushd ../../examples/$example
+		python3 robot.py test --builtin
+		popd
+	done
+fi
 
