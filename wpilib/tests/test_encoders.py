1311601687dabf16c5e992b1743c2a8924a8cace
Christian J. Balcom <robot.inventor@gmail.com>
2015-01-14 21:42:51 -0500

    Add setEncoderIndexSource to Encoder

--- wpilib/tests/test_encoders.py
+++ wpilib/tests/test_encoders.py
@@ -18,10 +18,18 @@ def encoder_data(hal_data):
 #port_b_pin
 #port_b_analog_trigger False
 
+def check_config(config, a_mod, a_pin, a_atr, b_mod, b_pin, b_atr):
+    assert config["ASource_Module"] == a_mod
+    assert config["ASource_Channel"] == a_pin
+    assert config["ASource_AnalogTrigger"] == a_atr
+    assert config["BSource_Module"] == b_mod
+    assert config["BSource_Channel"] == b_pin
+    assert config["BSource_AnalogTrigger"] == b_atr
+
 def test_channel_channel_init(wpilib, encoder_data):
     x = wpilib.Encoder(1, 2)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == False
     x.free()
     assert encoder_data["initialized"] == False
@@ -30,7 +38,7 @@ def test_channel_channel_init(wpilib, encoder_data):
 def test_channel_channel_reverse_init(wpilib, encoder_data):
     x = wpilib.Encoder(1, 2, True)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     assert encoder_data["initialized"] == False
@@ -39,7 +47,7 @@ def test_channel_channel_reverse_init(wpilib, encoder_data):
 def test_channel_channel_reverse_type_init(wpilib, encoder_data):
     x = wpilib.Encoder(1, 2, True, wpilib.Encoder.EncodingType.k4X)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     assert encoder_data["initialized"] == False
@@ -48,7 +56,7 @@ def test_channel_channel_reverse_type_init(wpilib, encoder_data):
 def test_channel_channel_channel_reverse_init(wpilib, encoder_data):
     x = wpilib.Encoder(1, 2, 3, True)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     assert encoder_data["initialized"] == False
@@ -57,7 +65,7 @@ def test_channel_channel_channel_reverse_init(wpilib, encoder_data):
 def test_channel_channel_channel_init(wpilib, encoder_data):
     x = wpilib.Encoder(1, 2, 3)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == False
     x.free()
     assert encoder_data["initialized"] == False
@@ -68,7 +76,7 @@ def test_source_source_reverse_init(wpilib, encoder_data):
     s2 = wpilib.DigitalInput(2)
     x = wpilib.Encoder(s1, s2, True)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     s1.free()
@@ -81,7 +89,7 @@ def test_source_source_init(wpilib, encoder_data):
     s2 = wpilib.DigitalInput(2)
     x = wpilib.Encoder(s1, s2)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == False
     x.free()
     s1.free()
@@ -94,7 +102,7 @@ def test_source_source_reverse_type_init(wpilib, encoder_data):
     s2 = wpilib.DigitalInput(2)
     x = wpilib.Encoder(s1, s2, True, wpilib.Encoder.EncodingType.k4X)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     s1.free()
@@ -108,7 +116,7 @@ def test_source_source_source_reverse_init(wpilib, encoder_data):
     s3 = wpilib.DigitalInput(3)
     x = wpilib.Encoder(s1, s2, s3, True)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == True
     x.free()
     s1.free()
@@ -123,7 +131,7 @@ def test_source_source_source_init(wpilib, encoder_data):
     s3 = wpilib.DigitalInput(3)
     x = wpilib.Encoder(s1, s2, s3)
     assert encoder_data["initialized"] == True
-    assert encoder_data['config'] == [0, 1, False, 0, 2, False]
+    check_config(encoder_data["config"], 0, 1, False, 0, 2, False)
     assert encoder_data['reverse_direction'] == False
     x.free()
     s1.free()
