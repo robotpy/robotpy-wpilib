a770c27d88e36827e61a47d0d137cf7df1870450
Dustin Spicuzza <dustin@virtualroadside.com>
2015-02-05 02:10:29 -0500

    Bump networktables requirement

--- wpilib/setup.py
+++ wpilib/setup.py
@@ -49,7 +49,7 @@ setup(
     url='https://github.com/robotpy/robotpy-wpilib',
     keywords='frc first robotics wpilib',
     packages=find_packages(),
-    install_requires=['pynetworktables>=2015.1.0'],
+    install_requires=['pynetworktables>=2015.1.2'],
     license="BSD License",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
