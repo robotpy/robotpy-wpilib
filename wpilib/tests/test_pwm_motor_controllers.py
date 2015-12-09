906fe5172f63b449df127189089d4a6775526c88
Dustin Spicuzza <dustin@virtualroadside.com>
2015-12-09 00:26:08 -0500

    More comprehensive testing of pwm values

--- wpilib/tests/test_pwm_motor_controllers.py
+++ wpilib/tests/test_pwm_motor_controllers.py
@@ -17,22 +17,20 @@ def test_controller(wpilib, hal_data, hal_impl_pwm_helpers, clsname):
     
     # Assert starts with zero
     assert obj.get() == 0
-    
+        
     # call set
-    obj.set(1)
+    for i in [-1, -.9, -.7, -.3, 0, .3, .7, .9, 1]:
+        obj.set(i)
+        
+        # validate that get returns the correct value
+        assert abs(obj.get() - i) < 0.01
+        
+        obj.pidWrite(i)
+        assert abs(obj.get() - i) < 0.01
+        
+        # validate that the helpers return the correct value and correct value stored in 'value'
+        # .. to about 0.01
+        assert abs(hal_impl_pwm_helpers.reverseByType(clsname ,hal_data['pwm'][2]['raw_value']) - i) < 0.01
+        assert abs(hal_impl_pwm_helpers.reverseByType(2) - i) < 0.01
+        assert abs(hal_data['pwm'][2]['value'] - i) < 0.01
     
-    # validate that get returns the correct value
-    assert obj.get() == 1
-    
-    # validate that the helpers return the correct value and correct value stored in 'value'
-    assert hal_impl_pwm_helpers.reverseByType(clsname ,hal_data['pwm'][2]['raw_value']) == 1
-    assert hal_impl_pwm_helpers.reverseByType(2) == 1
-    assert hal_data['pwm'][2]['value'] == 1
-    
-    # pidWrite
-    obj.pidWrite(-1)
-    
-    assert obj.get() == -1
-    assert hal_impl_pwm_helpers.reverseByType(clsname ,hal_data['pwm'][2]['raw_value']) == -1
-    assert hal_impl_pwm_helpers.reverseByType(2) == -1
-    assert hal_data['pwm'][2]['value'] == -1
