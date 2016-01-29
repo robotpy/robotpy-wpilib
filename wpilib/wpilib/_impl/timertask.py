321d39f6cb55ce465fd2187144b59c220fbe5fbf
Dustin Spicuzza <dustin@virtualroadside.com>
2016-01-29 17:56:30 -0500

    Modify timertask to be resilient to falling behind

--- wpilib/wpilib/_impl/timertask.py
+++ wpilib/wpilib/_impl/timertask.py
@@ -7,13 +7,17 @@
 import threading
 from ..timer import Timer
 
+import logging
 
 class TimerTask(threading.Thread):
     
     def __init__(self, name, period, task_fn):
         super().__init__(name=name, daemon=True)
+        self.logger = logging.getLogger('wpilib.%s' % name)
         self.period = period
         self.task_fn = task_fn
+        
+        self.last_warning = -10
         
         self.stopped = False
     
@@ -23,18 +27,30 @@ class TimerTask(threading.Thread):
     
     def run(self):
         
-        period = self.period
-        wait_til = Timer.getFPGATimestamp() + period
+        self.logger.info("Timer task started")
         
-        while not self.stopped:
+        try:
+            period = self.period
+            wait_til = Timer.getFPGATimestamp() + period
             
-            now = Timer.getFPGATimestamp()
-            Timer.delay(wait_til - now)
-            
-            if self.stopped:
-                break
-            
-            self.task_fn()
-            
-            wait_til += period
-    
+            while not self.stopped:
+                
+                now = Timer.getFPGATimestamp()
+                delay = wait_til - now
+                
+                if delay > 0:
+                    Timer.delay(wait_til - now)
+                else:
+                    # only emit warning once per second
+                    if now - self.last_warning > 1:
+                        self.last_warning = now
+                        self.logger.warn("Delay time was negative (%02f: too much going on?)", delay)
+                
+                if self.stopped:
+                    break
+                
+                self.task_fn()
+                
+                wait_til += period
+        finally:
+            self.logger.info("Timer task exited")
