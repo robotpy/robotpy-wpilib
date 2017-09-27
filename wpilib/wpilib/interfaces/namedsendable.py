cf3a401144daded16816956d78b3efb1053bbdfc
Arthur Allshire <arthur.allshire@gmail.com>
2017-09-27 10:32:58 +1000

    Validate namedsendable.py

--- wpilib/wpilib/interfaces/namedsendable.py
+++ wpilib/wpilib/interfaces/namedsendable.py
@@ -1,4 +1,4 @@
-# validated: 2015-12-22 DS 6d854af edu/wpi/first/wpilibj/NamedSendable.java
+# validated: 2017-09-27 AA e1195e8b9dab edu/wpi/first/wpilibj/NamedSendable.java
 from ..sendable import Sendable
 
 __all__ = ["NamedSendable"]
