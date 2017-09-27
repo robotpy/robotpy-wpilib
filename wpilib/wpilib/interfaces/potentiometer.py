7638fcddc0f14012cfd6c886b73595d3c20d21fb
Arthur Allshire <arthur.allshire@gmail.com>
2017-09-27 10:33:23 +1000

    Validate potentiometer.py

--- wpilib/wpilib/interfaces/potentiometer.py
+++ wpilib/wpilib/interfaces/potentiometer.py
@@ -1,4 +1,4 @@
-# validated: 2015-12-22 DS 6d854af edu/wpi/first/wpilibj/interfaces/Potentiometer.java
+# validated: 2017-09-27 AA e1195e8b9dab edu/wpi/first/wpilibj/interfaces/Potentiometer.java
 #----------------------------------------------------------------------------
 # Copyright (c) FIRST 2008-2014. All Rights Reserved.
 # Open Source Software - may be modified and shared by FRC teams. The code
