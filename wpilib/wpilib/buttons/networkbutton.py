9376241a7009f985e05b137803af3202c9f4f3cc
Arthur Allshire <arthur.allshire@gmail.com>
2017-10-19 13:08:46 +1100

    Update and validate networkbutton.py

--- wpilib/wpilib/buttons/networkbutton.py
+++ wpilib/wpilib/buttons/networkbutton.py
@@ -1,6 +1,6 @@
-# validated: 2015-12-22 DS 6d854af edu/wpi/first/wpilibj/buttons/NetworkButton.java
+# validated: 2017-10-19 AA 4e80570c4c48 edu/wpi/first/wpilibj/buttons/NetworkButton.java
 #----------------------------------------------------------------------------
-# Copyright (c) FIRST 2008-2012. All Rights Reserved.
+# Copyright (c) FIRST 2008-2017. All Rights Reserved.
 # Open Source Software - may be modified and shared by FRC teams. The code
 # must be accompanied by the FIRST BSD license file in the root directory of
 # the project.
@@ -8,20 +8,30 @@
 
 from .button import Button
 
+from networktables.networktable import NetworkTable
+from networktables import NetworkTablesInstance
+
 __all__ = ["NetworkButton"]
+
 
 class NetworkButton(Button):
     """A :class:`.Button` that uses a :class:`NetworkTable` boolean field."""
 
     def __init__(self, table, field):
-        from networktables import NetworkTables
-        from networktables.networktable import NetworkTable
-        
+        """Initialize the NetworkButton.
+
+        :param table: the NetworkTable instance to use, or the name of the
+        table to use.
+        :param field: field to use.
+        """
+
         if isinstance(table, NetworkTable):
-            self.table = table
+            self.entry = table.getEntry(field)
         else:
-            self.table = NetworkTables.getTable(table)
-        self.field = field
+            table = NetworkTablesInstance.getDefault().getTable(table)
+            self.entry = table.getEntry(field)
 
     def get(self):
-        return self.table.isConnected() and self.table.getBoolean(self.field, False)
+        """Get the value of the button."""
+        return (self.entry.getInstance().isConnected()
+                and self.entry.getBoolean(False))
