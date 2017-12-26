ffb2514fb14f72a8266939a013d97df27b7f6bee
Dustin Spicuzza <dustin@virtualroadside.com>
2017-12-25 23:43:58 -0500

    Update SPI and fix related tests

--- hal-sim/hal_impl/spi_helpers.py
+++ hal-sim/hal_impl/spi_helpers.py
@@ -2,59 +2,61 @@
 from . import data
 hal_data = data.hal_data
 
+from . import functions
+
 class SPISimBase:
     '''
         Base class to use for SPI protocol simulators.
-        
+
         Has all functions that need to be implemented, but throws exceptions
         when data is asked of it. Will throw away set* function data, as most
-        low-fidelity simulation will probably not care about such things. 
+        low-fidelity simulation will probably not care about such things.
     '''
-    
+
     def initializeSPI(self, port, status):
         self.port = port
         status.value = 0
-    
+
     def transactionSPI(self, port, dataToSend, dataReceived, size):
         '''
             To give data back use ``data_received``::
-            
+
                 data_received[:] = [1,2,3...]
-            
+
             :returns: number of bytes returned
         '''
         raise NotImplementedError
-    
+
     def writeSPI(self, port, dataToSend, sendSize):
         ''':returns: number of bytes written'''
         return sendSize
-    
+
     def readSPI(self, port, buffer, count):
         '''
             To give data, do ``buffer[:] = [1,2,3...]``
-            
+
             :returns: number of bytes read
         '''
         raise NotImplementedError
-    
+
     def closeSPI(self, port):
         pass
-    
+
     def setSPISpeed(self, port, speed):
         pass
-    
+
     def setSPIOpts(self, port, msbFirst, sampleOnTrailing, clkIdleHigh):
         pass
-    
+
     def setSPIChipSelectActiveHigh(self, port, status):
         pass
-    
+
     def setSPIChipSelectActiveLow(self, port, status):
         pass
-    
+
     def getSPIHandle(self, port):
         pass
-    
+
     def setSPIHandle(self, port, handle):
         pass
 
@@ -92,35 +94,38 @@ class SPISimBase:
 class ADXRS450_Gyro_Sim(SPISimBase):
     '''
         This returns the angle of the gyro as the value of::
-            
+
             hal_data['robot']['adxrs450_spi_%d_angle']
-            
+
         Where %d is the i2c port number. Angle should be in degrees.
     '''
-    
+
     def __init__(self, gyro):
         self.kDegreePerSecondPerLSB = gyro.kDegreePerSecondPerLSB
         self.kSamplePeriod = gyro.kSamplePeriod
-    
+        self.lastAngle = 0
+
     def initializeSPI(self, port, status):
         self.angle_key = 'adxrs450_spi_%d_angle' % port
         self.rate_key = 'adxrs450_spi_%d_rate' % port
-        
-        print(self.angle_key)
-    
-    def getSPIAccumulatorAverage(self, port, status):
-        return 0
-    
-    def getSPIAccumulatorValue(self, port, status):
-        return int(hal_data['robot'].get(self.angle_key, 0) / (self.kDegreePerSecondPerLSB * self.kSamplePeriod))
-    
-    def getSPIAccumulatorLastValue(self, port, status):
-        return int(hal_data['robot'].get(self.rate_key, 0) / self.kDegreePerSecondPerLSB)
-    
-    # for calibrate
-    def writeSPI(self, port, data_to_send, send_size):
-        return send_size
-    
+
+    def setSPIAutoTransmitData(self, port, data_to_send, sendSize, zeroSize, status):
+        status.value = 0
+
+    def readSPIAutoReceivedData(self, port, buffer, numToRead, timeout, status):
+        ''':returns: number of bytes read'''
+        status.value = 0
+
+        current = hal_data['robot'].get(self.angle_key, 0)
+
+        if numToRead != 0:
+            offset = current - self.lastAngle
+            self.lastAngle = current
+            offset = int(offset / (self.kSamplePeriod * self.kDegreePerSecondPerLSB))
+            buffer[0:4] = offset.to_bytes(4, 'big', signed=True)
+
+        return 4
+
     def readSPI(self, port, buffer, count):
         buffer[:] = (0xff000000 | (0x5200 << 5)).to_bytes(4, 'big')
         return count
--- wpilib/tests/test_spi.py
+++ wpilib/tests/test_spi.py
@@ -5,7 +5,7 @@ import hal
 from hal_impl.spi_helpers import SPISimBase
 
 class SPISimulator(SPISimBase):
-    
+
     def transactionSPI(self, port, data_to_send, data_received, size):
         assert port == self.port
         assert list(data_to_send) == [1, 2, 3]
@@ -13,28 +13,28 @@ class SPISimulator(SPISimBase):
         data_received[:] = [3,2,1]
         self.tx = True
         return 3
-    
+
     def writeSPI(self, port, data_to_send, send_size):
         assert port == self.port
         assert list(data_to_send) == [5,6,7]
         assert send_size == 3
         self.wrote = True
         return 3
-    
+
     def readSPI(self, port, buffer, count):
         assert port == self.port
         buffer[:] = [9, 9, 0]
         self.did_read = True
         return 3
-    
+
     def closeSPI(self, port):
         self.closed = True
-    
+
     def setSPIChipSelectActiveHigh(self, port, status):
         assert port == self.port
         status.value = 0
         self.set_active_high = True
-    
+
     def setSPIChipSelectActiveLow(self, port, status):
         assert port == self.port
         status.value = 0
@@ -52,26 +52,26 @@ def test_spi(wpilib, monkeypatch):
 
     sim = SPISimulator()
     port = wpilib.SPI.Port.kMXP
-    
+
     spi = wpilib.SPI(port, sim)
     assert sim.port == port
-    
+
     spi.setChipSelectActiveHigh()
     assert sim.set_active_high == True
-    
+
     spi.setChipSelectActiveLow()
     assert sim.set_active_low == True
-    
+
     assert spi.transaction([1,2,3]) == [3,2,1]
     assert sim.tx == True
-    
+
     assert spi.write([5,6,7]) == 3
     assert sim.wrote == True
-    
+
     assert spi.read(False, 3) == [9, 9, 0]
     assert sim.did_read == True
-    
-    
+
+
     spi.freeAccumulator()
     assert sim.acc_freed == True
 
@@ -83,19 +83,26 @@ def test_spi(wpilib, monkeypatch):
 
     spi.free()
     assert sim.closed == True
-    
+
     with pytest.raises(ValueError):
         spi.port
-    
 
-def test_adxrs450(wpilib, hal_data):
-    
+
+def test_adxrs450(wpilib, hal_data, monkeypatch, sim_hooks):
+
+    monkeypatch.setattr(wpilib.Notifier, '_thread', lambda s: None)
+
     gyro = wpilib.ADXRS450_Gyro()
-    
+
     hal_data['robot']['adxrs450_spi_0_angle'] = 10
-    assert abs(gyro.getAngle() - 10) < 0.001
-    
-    hal_data['robot']['adxrs450_spi_0_rate'] = 5
-    assert abs(gyro.getRate() - 5) < 0.001
-    
-    
+
+    for i in range(10):
+        sim_hooks.time = i*.1
+        assert gyro.getAngle() == pytest.approx(10)
+
+    hal_data['robot']['adxrs450_spi_0_angle'] = -5
+    sim_hooks.time = 2
+    assert gyro.getAngle() == pytest.approx(-5)
+
+    # TODO
+    #assert gyro.getRate() == pytest.approx(5)
