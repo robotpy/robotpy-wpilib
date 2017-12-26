f19c9a5593809841119dbe88d0580a55df6a7a48
Tim Winters <twinters007@gmail.com>
2017-12-26 02:07:28 -0500

    Update drive related functions

--- wpilib/wpilib/drive/mecanumdrive.py
+++ wpilib/wpilib/drive/mecanumdrive.py
@@ -1,6 +1,7 @@
-# validated: 2017-10-23 TW 19addb04cf4a edu/wpi/first/wpilibj/drive/MecanumDrive.java
-import hal
+# validated: 2017-12-25 TW f9bece2ffbf7 edu/wpi/first/wpilibj/drive/MecanumDrive.java
 import math
+
+import hal
 from .robotdrivebase import RobotDriveBase
 from .vector2d import Vector2d
 
@@ -27,7 +28,27 @@ class MecanumDrive(RobotDriveBase):
     Each drive() function provides different inverse kinematic relations for a Mecanum drive
     robot. Motor outputs for the right side are negated, so motor direction inversion by the user is
     usually unnecessary.
+
+    This library uses the NED axes convention (North-East-Down as external reference in the world
+    frame): http://www.nuclearprojects.com/ins/images/axis_big.png.
+
+    The positive X axis points ahead, the positive Y axis points right, and the positive Z axis
+    points down. Rotations follow the right-hand rule, so clockwise rotation around the Z axis is
+    positive.
+
+    RobotDrive porting guide:
+    In MecanumDrive, the right side speed controllers are automatically inverted, while in RobotDrive,
+    no speed controllers are automatically inverted.
+
+    :meth:`driveCartesian` is equivalent to :meth:`.RobotDrive.mecanumDrive_Cartesian` if a deadband of 0
+    is used, and the ``ySpeed`` and ``gyroAngle`` values are inverted compared to ``RobotDrive`` (i.e
+    ``driveCartesian(xSpeed, -ySpeed, zRotation, -gyroAngle)``.
+
+    :meth:`drivePolar` is equivalent to :meth:`.RobotDrive.mecanumPolar` if a deadband of 0 is used.
     """
+
+    instances = 0
+
     def __init__(self, frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor):
         """Constructor for MecanumDrive.
 
@@ -47,16 +68,25 @@ class MecanumDrive(RobotDriveBase):
         self.frontRightMotor = frontRightMotor
         self.rearRightMotor = rearRightMotor
 
+        self.addChild(self.frontLeftMotor)
+        self.addChild(self.rearLeftMotor)
+        self.addChild(self.frontRightMotor)
+        self.addChild(self.rearRightMotor)
+        MecanumDrive.instances += 1
+        self.setName("MecanumDrive", self.instances)
+
         self.reported = False
 
-    def driveCartesian(self, x, y, rotation, gyroAngle=0.0):
+    def driveCartesian(self, ySpeed, xSpeed, zRotation, gyroAngle=0.0):
         """Drive method for Mecanum platform.
 
-        :param x: The speed that the robot should drive in the X direction. ``[-1.0..1.0]``
-        :param y: The speed that the robot should drive in the Y direction. ``[-1.0..1.0]``
-        :param rotation: The rate of rotation for the robot that is completely independent of the
-                         translation. ``[-1.0..1.0]``
-        :param gyroAngle: The current angle reading from the gyro in degrees around the Z axis. Use 
+        Angles are measured clockwise from the positive X axis. The robot's speed is independent
+        from its angle or rotation rate.
+
+        :param ySpeed: The robot's speed along the Y axis [-1.0..1.0]. Right is positive.
+        :param xSpeed: The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+        :param zRotation: The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is positive.
+        :param gyroAngle: The current angle reading from the gyro in degrees around the Z axis. Use
                           this to implement field-oriented controls.
         """
         if not self.reported:
@@ -65,25 +95,25 @@ class MecanumDrive(RobotDriveBase):
                        hal.UsageReporting.kRobotDrive_MecanumCartesian)
             self.reported = True
 
-        x = RobotDriveBase.limit(x)
-        x = RobotDriveBase.applyDeadband(x, self.deadband)
+        ySpeed = RobotDriveBase.limit(ySpeed)
+        ySpeed = RobotDriveBase.applyDeadband(ySpeed, self.deadband)
 
-        y = RobotDriveBase.limit(y)
-        y = RobotDriveBase.applyDeadband(y, self.deadband)
+        xSpeed = RobotDriveBase.limit(xSpeed)
+        xSpeed = RobotDriveBase.applyDeadband(xSpeed, self.deadband)
 
         # Compensate for gyro angle
-        input = Vector2d(x, y)
+        input = Vector2d(ySpeed, xSpeed)
         input.rotate(gyroAngle)
 
         wheelSpeeds = [
             # Front Left
-            input.x + input.y + rotation,
+            input.x + input.y + zRotation,
             # Rear Left
-            -input.x + input.y + rotation,
+            -input.x + input.y + zRotation,
             # Front Right
-            input.x - input.y + rotation,
+            input.x - input.y + zRotation,
             # Rear Right
-            -input.x - input.y + rotation
+            -input.x - input.y + zRotation
         ]
 
         RobotDriveBase.normalize(wheelSpeeds)
@@ -97,14 +127,16 @@ class MecanumDrive(RobotDriveBase):
 
         self.feed()
 
-    def drivePolar(self, magnitude, angle, rotation):
+    def drivePolar(self, magnitude, angle, zRotation):
         """Drive method for Mecanum platform.
 
-        :param magnitude: The speed that the robot should drive in a given direction. ``[-1.0..1.0]``
-        :param angle: The direction the robot should drive in degrees. 0.0 is straight ahead. The
-                      direction and maginitude are independent of the rotation rate.
-        :param rotation: The rate of rotation for the robot that is completely independent of the
-                         magnitude or direction. ``[-1.0..1.0]``
+        Angles are measured counter-clockwise from straight ahead. The speed at which the robot
+        drives (translation) is independent from its angle or rotation rate.
+
+        :param magnitude: The robot's speed at a given angle [-1.0..1.0]. Forward is positive.
+        :param angle: The angle around the Z axis at which the robot drives in degrees [-180..180].
+        :param zRotation: The robot's rotation rate around the Z axis [-1.0..1.0]. Clockwise is
+                          positive.
         """
         if not self.reported:
             hal.report(hal.UsageReporting.kResourceType_RobotDrive,
@@ -116,7 +148,7 @@ class MecanumDrive(RobotDriveBase):
 
         self.driveCartesian(magnitude * math.cos(math.radians(angle)),
                             magnitude * math.sin(math.radians(angle)),
-                            rotation, 0.0)
+                            zRotation, 0.0)
 
     def stopMotor(self):
         self.frontLeftMotor.stopMotor()
@@ -127,3 +159,10 @@ class MecanumDrive(RobotDriveBase):
 
     def getDescription(self):
         return "Mecanum Drive"
+
+    def initSendable(self, builder):
+        builder.setSmartDashboardType("MecanumDrive")
+        builder.addDoubleProperty("Front Left Motor Speed", self.frontLeftMotor.get, self.frontLeftMotor.set)
+        builder.addDoubleProperty("Front Right Motor Speed", self.frontRightMotor.get, self.frontRightMotor.set)
+        builder.addDoubleProperty("Rear Left Motor Speed", self.rearLeftMotor.get, self.rearLeftMotor.set)
+        builder.addDoubleProperty("Rear Right Motor Speed", self.rearRightMotor.get, self.rearRightMotor.set)
