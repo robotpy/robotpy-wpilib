2d28e36dcf8144edbb2d70323393636dc951b1a1
ariovistus <ellery-newcomer@utulsa.edu>
2017-12-31 17:41:15 -0800

    Fix Notifier.stop() (#399)

--- wpilib/tests/test_notifier.py
+++ wpilib/tests/test_notifier.py
@@ -1,6 +1,7 @@
 
 import threading
 
+
 def test_notifier_single(wpilib):
     
     c = threading.Condition()
@@ -13,6 +14,22 @@ def test_notifier_single(wpilib):
         n.startSingle(0.05)
         assert c.wait(timeout=1.0)
     
+    n.free()
+
+
+def test_notifier_periodic(wpilib):
+    
+    c = threading.Condition()
+    def _fn():
+        with c:
+            c.notify_all()
+    
+    n = wpilib.Notifier(run=_fn)
+    with c:
+        n.startPeriodic(0.05)
+        assert c.wait(timeout=1.0)
+        assert c.wait(timeout=1.0)
+        n.stop()
     n.free()
 
 # TODO: better tests
