9194444c3f08b3a4f0f96e9dae53867083407fb3
Dustin Spicuzza <dustin@virtualroadside.com>
2018-01-17 00:32:43 -0500

    logging: don't fail when no traceback is present

--- wpilib/wpilib/_impl/logconfig.py
+++ wpilib/wpilib/_impl/logconfig.py
@@ -42,18 +42,18 @@ class VerboseExceptionFormatter(logging.Formatter):
         # Now we're going to format and add the locals information.
         output_lines = [exc_text, '\n']
         tb = exc_info[2]  # This is the outermost frame of the traceback.
-        while tb.tb_next:
-            tb = tb.tb_next  # Zoom to the innermost frame.
-        output_lines.append('Locals at innermost frame:\n')
-        locals_text = pprint.pformat(tb.tb_frame.f_locals, indent=2)
-        locals_lines = locals_text.split('\n')
-        if len(locals_lines) > MAX_VARS_LINES:
-            locals_lines = locals_lines[:MAX_VARS_LINES]
-            locals_lines[-1] = '...'
-        output_lines.extend(
-            line[:MAX_LINE_LENGTH - 3] + '...' if len(line) > MAX_LINE_LENGTH else line
-            for line in locals_lines)
-        output_lines.append('\n')
+        if tb: # this should always be true, but sometimes someone messes up
+            while tb.tb_next:
+                tb = tb.tb_next  # Zoom to the innermost frame.
+            output_lines.append('Locals at innermost frame:\n')
+            locals_text = pprint.pformat(tb.tb_frame.f_locals, indent=2)
+            locals_lines = locals_text.split('\n')
+            if len(locals_lines) > MAX_VARS_LINES:
+                locals_lines = locals_lines[:MAX_VARS_LINES]
+                locals_lines[-1] = '...'
+            output_lines.extend(
+                line[:MAX_LINE_LENGTH - 3] + '...' if len(line) > MAX_LINE_LENGTH else line
+                for line in locals_lines)
+            output_lines.append('\n')
         return '\n'.join(output_lines)
     
-
