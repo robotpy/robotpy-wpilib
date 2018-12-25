9ef64f0a89dc2f7db5c4dc23d84a6184afb41f23
David Vo <auscompgeek@users.noreply.github.com>
2018-12-25 15:07:13 +1100

    Update Watchdog to use a single thread
    Also fix a couple of logging-related format string bugs.

--- wpilib/tests/test_watchdog.py
+++ wpilib/tests/test_watchdog.py
@@ -8,24 +8,11 @@ def MockThread():
         yield Thread
 
 
-def test_init1(wpilib, sim_hooks, MockThread):
-    sim_hooks.time = 4.5
-    doggo = wpilib.Watchdog(2)
-
-    assert doggo.timeout == 2
-    assert doggo.callback is not None
-    assert doggo.startTime == 4.5
-    assert not doggo.isExpired()
-    assert doggo.getTime() == 0
-
-
-def test_init2(wpilib, sim_hooks, MockThread):
-    sim_hooks.time = 4.5
+def test_init2(wpilib, MockThread):
     cb = MagicMock()
     doggo = wpilib.Watchdog(2, cb)
 
-    assert doggo.timeout == 2
-    assert doggo.callback == cb
-    assert doggo.startTime == 4.5
+    assert doggo._timeout == 2000000
+    assert isinstance(doggo._timeout, int)
+    assert doggo._callback is cb
     assert not doggo.isExpired()
-    assert doggo.getTime() == 0
