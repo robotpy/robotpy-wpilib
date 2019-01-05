8c2385209083841110ffed1aab5184c61533420f
Dustin Spicuzza <dustin@virtualroadside.com>
2019-01-05 11:30:05 -0500

    Bump requirements for release

--- wpilib/setup.py
+++ wpilib/setup.py
@@ -60,7 +60,7 @@ setup(
     url="https://github.com/robotpy/robotpy-wpilib",
     keywords="frc first robotics wpilib",
     packages=find_packages(),
-    install_requires=["pynetworktables>=2018.1.0"],
+    install_requires=["pynetworktables>=2019.0.0"],
     license="BSD License",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
