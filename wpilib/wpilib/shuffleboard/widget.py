05c2bd189b65dc7b08423d40a6f2bc6ba1d958d2
Tim Winters <twinters@wpi.edu>
2019-01-05 17:47:09 -0500

    Shuffleboard updates

--- wpilib/wpilib/shuffleboard/widget.py
+++ wpilib/wpilib/shuffleboard/widget.py
@@ -1,12 +1,19 @@
-# validated: 2018-11-18 EN 175c6c1f0130 edu/wpi/first/wpilibj/shuffleboard/ShuffleboardWidget.java
+# validated: 2019-01-05 TW 01d13220660c edu/wpi/first/wpilibj/shuffleboard/ShuffleboardWidget.java
 # ----------------------------------------------------------------------------
 # Copyright (c) 2018 FIRST. All Rights Reserved.
 # Open Source Software - may be modified and shared by FRC teams. The code
 # must be accompanied by the FIRST BSD license file in the root directory of
 # the project.
 # ----------------------------------------------------------------------------
+from typing import Union
+
 from .component import ShuffleboardComponent
 from .container import ShuffleboardContainer
+
+
+class WidgetType:
+    def getWidgetName(self) -> str:
+        pass
 
 
 class ShuffleboardWidget(ShuffleboardComponent):
@@ -17,7 +24,7 @@ class ShuffleboardWidget(ShuffleboardComponent):
     def __init__(self, parent: ShuffleboardContainer, title: str):
         super().__init__(parent, title)
 
-    def withWidget(self, widgetType) -> "ShuffleboardWidget":
+    def withWidget(self, widgetType: Union[WidgetType, str]) -> "ShuffleboardWidget":
         """
         Sets the type of widget used to display the data. If not set, the 
         default widget type will be used.
@@ -25,5 +32,8 @@ class ShuffleboardWidget(ShuffleboardComponent):
         :param widgetType: the type of the widget used to display the data
         :returns: this widget object
         """
-        self.setType(widgetType)
+        if isinstance(widgetType, WidgetType):
+            self.setType(widgetType.getWidgetName())
+        else:
+            self.setType(widgetType)
         return self
