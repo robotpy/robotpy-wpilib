a847a760426ebbdb8085fabca0d04512ddf4ec49
David Vo <auscompgeek@users.noreply.github.com>
2019-02-01 16:06:04 +1100

    spark: Fix docs

--- wpilib/wpilib/spark.py
+++ wpilib/wpilib/spark.py
@@ -1,6 +1,6 @@
 # validated: 2017-12-15 EN f9bece2ffbf7 edu/wpi/first/wpilibj/Spark.java
 # ----------------------------------------------------------------------------
-# Copyright (c) FIRST 2008-2012. All Rights Reserved.
+# Copyright (c) FIRST 2008-2018. All Rights Reserved.
 # Open Source Software - may be modified and shared by FRC teams. The code
 # must be accompanied by the FIRST BSD license file in the root directory of
 # the project.
@@ -23,17 +23,18 @@ class Spark(PWMSpeedController):
     def __init__(self, channel: int) -> None:
         """Constructor.
 
-        :param channel: The PWM channel that the SPARK is attached to. 0-9 are on-board, 10-19 are on the MXP port
-        
+        :param channel: The PWM channel that the SPARK is attached to.
+                        0-9 are on-board, 10-19 are on the MXP port
+
         .. note ::
-        
-           Note that the SD540 uses the following bounds for PWM values. These
+
+           The SPARK uses the following bounds for PWM values. These
            values should work reasonably well for most controllers, but if users
            experience issues such as asymmetric behavior around the deadband or
            inability to saturate the controller in either direction, calibration is
-           recommended. The calibration procedure can be found in the SD540 User
-           Manual available from Mindsensors.
-           
+           recommended. The calibration procedure can be found in the Spark User
+           Manual available from REV Robotics.
+
            - 2.003ms = full "forward"
            - 1.55ms = the "high end" of the deadband range
            - 1.50ms = center of the deadband range (off)
