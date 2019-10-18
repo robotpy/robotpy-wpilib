8438b9aa1012d30b80d41ffb04ba627e10e5c83c
Nick Sevryukov <nikeropiukiuk1324@gmail.com>
2019-10-18 15:41:40 +0300

    Remove Python 3.5 and add 3.7, 3.8 Trove classifiers

--- hal-base/setup.py
+++ hal-base/setup.py
@@ -60,8 +60,9 @@ setup(
         "Intended Audience :: Education",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering",
     ],
 )
