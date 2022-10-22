d4dfb7a4da22392a1dfdf142813439bff14d07e4
Dustin Spicuzza <dustin@virtualroadside.com>
2022-10-22 12:35:28 -0400

    Remove backwards compatibility stubs for wpimath

--- wpilib/geometry/__init__.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-warnings.warn(
-    "wpilib.geometry has moved to wpimath.geometry", FutureWarning, stacklevel=2
-)
-
-from wpimath.geometry import Pose2d, Rotation2d, Transform2d, Translation2d, Twist2d
-
-__all__ = ["Pose2d", "Rotation2d", "Transform2d", "Translation2d", "Twist2d"]
